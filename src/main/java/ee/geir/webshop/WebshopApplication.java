2cbf9600bf7f7a5bcbaf8146dc0b6a032001e0c7
geirtagapere <geir.tagapere@gmail.com>
2025-12-15 10:11:33 +0200

    project name

--- src/main/java/ee/geir/webshop/WebshopApplication.java
+++ src/main/java/ee/geir/webshop/WebshopApplication.java
@@ -13,9 +13,19 @@ public class WebshopApplication {
     // 1. 02.12 - Spring algus: andmebasaide yhendamine, controller, entity, repository
     // 2. 04.12 - korralikud veateated, tabelite omavaheline yhendamine, @Service, validations
     // 3. 09.12 - rendipood, DTO
-    // 4. 11.12 - unit testid
-    // 5. 16.12 - front-end
-    // 6. 18.12 - front-end
-    // 7. 23.12 - 9.00-12.15
-    // 8. 29.12 -
+    // 4. 11.12 - 13.00 unit testid, integration tests
+    // 5. 16.12 - 13.00 front-end
+    // 6.N 18.12 - 13.00 front-end
+    // 7.T 23.12 - 9.00-12.15
+    // 8.E 29.12 - 13.00
+    // 9.E 05.01 - 13.00
+    //10.N 08.01 - 13.00
+    //11.E 12.01 - 13.00
+    //12.N 15.01 - 13.00
+    //13.E 19.01 - 13.00
+    //14.N 22.01 - 13.00
+    //15.E 26.01 - 13.00
+    //16.N 29.01 - 13.00
+    //17.E 02.02 - 13.00
+    //18 lõpuprojekti päev
 }
--- src/main/java/ee/geir/webshop/exception/ControllerAdviceHandler.java
+++ src/main/java/ee/geir/webshop/exception/ControllerAdviceHandler.java
@@ -1,6 +1,5 @@
 package ee.geir.webshop.exception;
 
-import ee.geir.webshop.entity.Product;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
