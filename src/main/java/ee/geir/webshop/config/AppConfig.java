4af4152fc6464c2bd589b54236a3d4f238e78773
geirtagapere <geir.tagapere@gmail.com>
2026-02-02 17:02:09 +0200

    mail send fix

--- src/main/java/ee/geir/webshop/config/AppConfig.java
+++ src/main/java/ee/geir/webshop/config/AppConfig.java
@@ -3,11 +3,8 @@ package ee.geir.webshop.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Properties;
 
 @Configuration
 public class AppConfig {
@@ -22,22 +19,5 @@ public class AppConfig {
         return new RestTemplate();
     }
 
-    @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("sandbox.smtp.mailtrap.io");
-        mailSender.setPort(587);
-
-        mailSender.setUsername("2819248622b67f");
-        mailSender.setPassword("308b0cec98cab9");
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true"); // Helpful for debugging
-
-        return mailSender;
-    }
 
 }
